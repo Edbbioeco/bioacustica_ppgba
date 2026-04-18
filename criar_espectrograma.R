779900fbc3bd8fa3cd5e16e4d4d87082fcc11423
seuusername <seuemail@gmail.com>
2026-04-18 15:39:36 -0300

    Modificar a paleta do espectrograma para viridis inferno

--- criar_espectrograma.R
+++ criar_espectrograma.R
@@ -19,4 +19,5 @@ voc
 ## Criar espectrograma ----
 
 voc |> seewave::spectro(flim = c(0.65, 4),
-                        tlim = c(0.61, 0.91))
+                        tlim = c(0.61, 0.91),
+                        palette = viridis::inferno)
