b0b332579008fb321e206b85771da7ae26ad30bd
seuusername <seuemail@gmail.com>
2026-04-18 15:35:48 -0300

    Criar o espectrograma

--- criar_espectrograma.R
+++ criar_espectrograma.R
@@ -9,3 +9,14 @@ library(tidyverse)
 ## Importar ----
 
 voc <- tuneR::readWave("Boana atlantica.wav")
+
+## Visualizar ----
+
+voc
+
+# Espectrograma -----
+
+## Criar espectrograma ----
+
+voc |> seewave::spectro(flim = c(0.65, 4),
+                        tlim = c(0.61, 0.91))
