fe82c1036b10d0973151fb924143b2efdbc5a5f4
seuusername <seuemail@gmail.com>
2026-04-19 13:55:42 -0300

    Criar os dados de espectrograma

--- espectrograma_ggplot.R
+++ espectrograma_ggplot.R
@@ -15,3 +15,10 @@ voc <- tuneR::readWave("vocalizacao.wav")
 ## Visualizar ----
 
 voc
+
+# Espectrograma ----
+
+## Dados do espectrograma ----
+
+espectro <- voc |> seewave::spectro(flim = c(0, 0.75),
+                                    tlim = c(15, 16.75))
