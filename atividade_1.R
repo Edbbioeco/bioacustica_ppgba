e3abe8ee8aa4c0b5ff218ae693e8ce0ca83edfe4
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-04 19:57:18 -0300

    Valores do espectrograma da vocalização

--- atividade_1.R
+++ atividade_1.R
@@ -25,3 +25,14 @@ voc <- tuneR::readWave("C:/Users/LENOVO/OneDrive/Documentos/projeto mestrado/voc
 voc
 
 voc |> seewave::listen()
+
+# Valores acústicos ----
+
+## Valores do espectrograma ----
+
+### Criar ----
+
+voc |> seewave::spectro(flim = c(4.2, 5.25),
+                        wl = 8112,
+                        wn = "blackman",
+                        ovlp = 99)
