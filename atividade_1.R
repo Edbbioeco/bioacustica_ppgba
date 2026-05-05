9d4e98c6e5d24b0cae15beded5700747e8a14bb5
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-04 22:01:30 -0300

    Alterado o tom de cinza das linhas de grade

--- atividade_1.R
+++ atividade_1.R
@@ -109,7 +109,7 @@ gg_oscilo <- oscilo_df |>
   theme(axis.text = element_text(size = 17.5),
         axis.title = element_text(size = 20),
         panel.grid = element_line(linetype = "dashed",
-                                  color = "gray50",
+                                  color = "gray",
                                   linewidth = 1),
         panel.grid.minor = element_blank(),
         legend.text = element_text(size = 17.5),
