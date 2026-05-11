d5d3408a0f10aaca85c58154dfd082f81fb9860a
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-11 19:59:10 -0300

    Importar os dados

--- atividade_2.R
+++ atividade_2.R
@@ -7,3 +7,21 @@ library(readxl)
 library(ggpubr)
 
 library(ggview)
+
+# Dados ----
+
+## Importar ----
+
+dados <- purrr::map2(1:10,
+            rep(c("Mulher", "Homem"), each = 5),
+            \(id, gen){
+
+              readxl::read_xlsx("gosta_de_cafe.xlsx",
+                                sheet = id) |>
+                dplyr::mutate(dplyr::across(.cols = 5:11,
+                                            .fns = ~as.numeric(.)),
+                              dplyr::across(.cols = dplyr::contains("Freq"),
+                                            .fns = ~./1),
+                              ID = paste0(gen, " ", id))
+            }) |>
+  dplyr::bind_rows()
