4a01024e062ce00c5da42e7267464d7dcbd76d12
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-14 21:36:38 -0300

    Importar áudios

--- espec_oscilo_atividade_2.R
+++ espec_oscilo_atividade_2.R
@@ -5,3 +5,17 @@ library(tidyverse)
 library(tuneR)
 
 library(seewave)
+
+# Dados ----
+
+## Importar ----
+
+audios <- purrr::map(list.files(path = "pratica_diferenciacao/",
+                                full.names = TRUE),
+                     \(audio){
+
+                       tuneR::readWave(audio)
+
+                     }) |>
+  setNames(list.files(path = "pratica_diferenciacao/") |>
+             stringr::str_remove(".WAV"))
