efe5b1d5115d7ba72205a40fba6b093d8e4d58c6
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-18 16:49:45 -0300

    Trasnformar em shapefile

--- filtrar_coordenadas.R
+++ filtrar_coordenadas.R
@@ -20,3 +20,26 @@ dados <-read.csv2("gbif/0024949-260507073636908.csv")
 dados
 
 dados |> dplyr::glimpse()
+
+# Shapefile ----
+
+## Tratar e trasnformar em shapefile ----
+
+dados_sf <- dados |>
+  dplyr::mutate(dplyr::across(.cols = dplyr::contains("decimal"),
+                            .fns = ~as.numeric(.)),
+                decimalLongitude = dplyr::if_else(decimalLongitude < -50,
+                                                  decimalLongitude / 10,
+                                                  decimalLongitude),
+                decimalLatitude = dplyr::if_else(decimalLatitude < -20,
+                                                 decimalLatitude / 10,
+                                                 decimalLatitude)) |>
+  dplyr::filter(!decimalLatitude |> is.na() &
+                  !decimalLongitude |> is.na()) |>
+  sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"),
+               crs = 4674)
+
+dados_sf
+
+ggplot() +
+  geom_sf(data = dados_sf)
