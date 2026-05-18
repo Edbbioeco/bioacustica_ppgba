0be227b20469afc5dae6a616d9d1b756c9f8c846
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-18 19:56:16 -0300

    Importar e visualizar o shapefile da Caatinga

--- valores_variaveis_ambientais.R
+++ valores_variaveis_ambientais.R
@@ -15,3 +15,33 @@ library(geodata)
 library(elevatr)
 
 library(CDSE)
+
+# Dados ----
+
+## Localidades ----
+
+### Importar ----
+
+loc <- readr::read_csv("localidades.csv")
+
+### Visualizar ----
+
+loc
+
+loc |> dplyr::glimpse()
+
+## Shapefile da Caatinga ----
+
+### importar ----
+
+caa <- geobr::read_biomes() |>
+  dplyr::filter(name_biome == "Caatinga")
+
+## Visualizar ----
+
+caa
+
+ggplot() +
+  geom_sf(data = caa, color = "black", fill = "goldenrod") +
+  geom_point(data = loc,
+             aes(Longitude, Latitude))
