0b8571abea8dfc2914d1d051bcb285be1f4a14ee
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-19 01:13:46 -0300

    Importar e visualizar os dados dos valores ambientais

--- modelos_lineares.R
+++ modelos_lineares.R
@@ -11,3 +11,17 @@ library(broom)
 library(performance)
 
 library(ggview)
+
+# Dados ----
+
+## Variáveis ambientais ----
+
+### Importar ----
+
+var <- readxl::read_xlsx("valores_var.xlsx")
+
+### Visualizar ----
+
+var
+
+var |> dplyr::glimpse()
