d59b3bcf0956c86b1b355106757f0b4656543126
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-22 11:35:35 -0300

    Alterada as configurações de visualização da vocalização

--- gerar_sinal_acustico.R
+++ gerar_sinal_acustico.R
@@ -16,36 +16,66 @@ library(ggview)
 
 ## Importar ----
 
-scinax <- tuneR::readWave("Scinax fuscovarius.wav")
+scinax <- tuneR::readWave("Scinax x signatus.wav")
 
 ## Visualizar ----
 
 scinax
 
-scinax |> seewave::spectro(flim = c(0, 5),
-                           tlim = c(0, 6),
+scinax |> seewave::spectro(flim = c(0, 6),
+                           tlim = c(0, 5),
                            ovlp = 99,
                            wn = "blackman",
                            wl = 2048)
 
+scinax |> seewave::listen()
+
 # Sinal acústico ----
+
+## Silencio ----
+
+silencio <- tuneR::silence(duration = 0.6746,
+                           xunit = "time",
+                           samp.rate = scinax@samp.rate,
+                           pcm = TRUE,
+                           stereo = FALSE)
+
+silencio
 
 ## Gerar múltiplos sinais acústicos ----
 
-sinal <- do.call(tuneR::bind,
-                 purrr::map(1:9,
-                            \(id){
+sinal <- purrr::map(1:9,
+                    \(id){
 
-          sonicscrewdriver::sweptsine(f0 = 491.2,
-                                      f1 = 4657.4,
-                                      sweep.time = 0.0248)
+            sonicscrewdriver::sweptsine(f0 = 693.5,
+                                        f1 = 5185.9,
+                                        sweep.time = 0.0197,
+                                        samp.rate = scinax@samp.rate,
+                                        bit = 32,
+                                        mode = "linear")
 
-                })
-        )
+                    }) |>
+  purrr::reduce(\(som1, som2) tuneR::bind(som1, som2))
 
-sinal |> seewave::spectro(flim = c(0, 5),
+sinal
+
+sinal |> seewave::spectro(flim = c(0, 5.5),
                           ovlp = 99,
                           wn = "blackman",
                           wl = 2048)
 
 sinal |> seewave::listen()
+
+sinal_completo <- purrr::map(1:5, \(id){sinal}) |>
+  purrr::reduce(\(som1, som2) tuneR::bind(som1, silencio, som2)) %>%
+  tuneR::bind(silencio, ., silencio)
+
+sinal_completo
+
+sinal_completo |> seewave::listen()
+
+sinal_completo |> seewave::spectro(flim = c(0, 6),
+                                   ovlp = 99,
+                                   wn = "blackman",
+                                   wl = 2048)
+
