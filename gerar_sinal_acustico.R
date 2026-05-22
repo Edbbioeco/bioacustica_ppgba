62517195276743dac783e61f6c4d9c773ac1b774
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-22 10:16:46 -0300

    gerar o sinal acústico

--- gerar_sinal_acustico.R
+++ gerar_sinal_acustico.R
@@ -25,3 +25,18 @@ scinax |> seewave::spectro(flim = c(0, 5),
                            ovlp = 99,
                            wn = "blackman",
                            wl = 2048)
+
+# Sinal acústico ----
+
+## Gerar múltiplos sinais acústicos ----
+
+sinal <- do.call(tuneR::bind,
+                 purrr::map(1:9,
+                            \(id){
+
+          sonicscrewdriver::sweptsine(f0 = 491.2,
+                                      f1 = 4657.4,
+                                      sweep.time = 0.0248)
+
+                })
+        )
