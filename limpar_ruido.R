415e0348e1040dc95d880cb660c1f69b984c6f17
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-22 17:40:43 -0300

    Lista de pacotes do script

--- limpar_ruido.R
+++ limpar_ruido.R
@@ -11,3 +11,21 @@ library(viridis)
 library(ggview)
 
 library(patchwork)
+
+# Dados ----
+
+## Importar ----
+
+scinax <- tuneR::readWave("Scinax x signatus.wav")
+
+## Visualizar ----
+
+scinax
+
+scinax |> seewave::spectro(flim = c(0, 6),
+                           tlim = c(0, 3.5),
+                           ovlp = 99,
+                           wn = "blackman",
+                           wl = 2048)
+
+scinax |> seewave::listen()
