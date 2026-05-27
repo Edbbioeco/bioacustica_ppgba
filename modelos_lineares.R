9ca43f6606981295491eb1fe970ff75b7faa16f8
Edbbioeco <edsonbbiologia@gmail.com>
2026-05-27 17:42:13 -0300

    Ajustar a largura da coluna do modelo

--- modelos_lineares.R
+++ modelos_lineares.R
@@ -147,6 +147,8 @@ estatisticas <- purrr::map2(modelos, vars, \(modelos, nome){
 
 estatisticas |>
   flextable::flextable() |>
+  flextable::width(width = 1.5,
+                   j = 1) |>
   flextable::width(width = 1,
                    j = 2) |>
   flextable::align(align = "center", part = "all") |>
